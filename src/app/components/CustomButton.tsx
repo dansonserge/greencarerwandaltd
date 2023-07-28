f6b72adb1f56f532598a9e76fe3b8119ef6556b3
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-07-28 08:34:37 +0200

    completed designing the intro

--- src/app/components/CustomButton.tsx
+++ src/app/components/CustomButton.tsx
@@ -1,10 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
-const CustomButton = ({text}:{text: string}) => {
+const CustomButton = ({
+  text,
+  type,
+  icon,
+}: {
+  text: string;
+  type: string;
+  icon?: string;
+}) => {
   return (
-    <Link href={''} className="customButtonOne">{text}</Link>
-  )
-}
+    <>
+      {type === "normal" ? (
+        <Link href={""} className="customButtonOne">
+          {text}
+        </Link>
+      ) : (
+        <Link href={""} className="customButtonTwo flex gap-2  ">
+          <span className="gradient-accent-color font-bold">Contact us</span>
+          {icon && <Image src={icon} alt={text}/>}
+        </Link>
+      )}
+    </>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
--- src/app/components/IntroCarousel/index.tsx
+++ src/app/components/IntroCarousel/index.tsx
@@ -6,6 +6,7 @@ import SelectedDot from "@/assets/SelectedIntroDot.svg";
 import DotsSelector from "./DotsSelector";
 import BannerImageOne from "@/assets/BannerImageOne.svg"
 import CustomButton from "../CustomButton";
+import ContactUsIcon from "@/assets/contactUsIcon.svg"
 
 const IntroCarousel = () => {
   return (
@@ -13,22 +14,23 @@ const IntroCarousel = () => {
       <DotsSelector />
 
       <div className="flex my-16 mx-16 ml-40 items-center w-full justify-between">
-        <div className="">
+        <div className="flex-1">
           <div className="text-7xl font-black text-dark-blue">
             <p>Our Environment,</p>
             <p>Our <span className="gradient-accent-color">Concern</span></p>
           </div>
-          <div className="w-[700px] mt-14 intro-text mb-16">
+          <div className="mt-14 intro-text text-3xl mb-16">
             <span className="font-bold">Greencare Rwanda ltd </span>conducts research and develop the business
             models for waste management by supporting the public sector to
             convert the landfill into recycling plants to promote circular
             economy and environmental protection.
           </div>
-          <div className="flex">
-            <CustomButton text="About us"/>
+          <div className="flex gap-4">
+            <CustomButton text="About us" type={'normal'}/>
+            <CustomButton text="Contact us" type={'gradient'} icon={ContactUsIcon}/>
           </div>
         </div>
-        <div>
+        <div className="">
           <Image src={BannerImageOne} alt={"Banner Image One"}  width={840}/>
         </div>
       </div>
