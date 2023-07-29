3b3913f6f3e653389095e7a0cc6a11f64bf2677a
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-07-29 13:55:39 +0200

    added UI interaction to the products section

--- src/app/components/OurProducts/index.tsx
+++ src/app/components/OurProducts/index.tsx
@@ -15,25 +15,26 @@ const OurProducts = () => {
 
   const [selectedDot, setSelectedDot] = useState<number>(0);
 
+
+
   return (
     <div className="flex mx-20">
       <div className="w-[800px] mt-10">
         <div className="absolute z-10">
-        <div className="gradient-accent-color font-black text-2xl">Our products</div>
-        <div className="text-dark-blue font-black text-6xl z-10 w-12">{selectedProduct.productName}</div>
+        <div className="gradient-accent-color font-black text-2xl mb-5">Our products</div>
+        <div className="text-dark-blue font-black text-6xl z-10 w-12 h-32">{selectedProduct.productName}</div>
         <div>
           <div className="flex gap-5 m-5">
-            {Array(3)
-              .fill("")
-              .map((item, index) =>
-                index === selectedDot ? (
+            {products.map((item, index) =>
+                item.productName === selectedProduct.productName ? (
                   <Image
                     key={index}
                     src={SelectedDot}
                     alt={""}
                     width={15}
                     height={15}
-                    onClick={() => setSelectedDot(index)}
+                    onClick={() => setSelectedProduct(item)}
+                    className="cursor-pointer"
                   />
                 ) : (
                   <Image
@@ -42,7 +43,8 @@ const OurProducts = () => {
                     alt={""}
                     width={15}
                     height={15}
-                    onClick={() => setSelectedDot(index)}
+                    onClick={() => setSelectedProduct(item)}
+                    className="cursor-pointer"
                   />
                 )
               )}
@@ -57,7 +59,7 @@ const OurProducts = () => {
 
         </div>
       </div>
-      <div className=" h-[510px] relative flex flex-col grow gap-16 pt-14 px-24 gcr-rounded-products-wrapper gcr-rounded-wrapper border border-gray-900 mb-12 bg-light-grey">
+      <div className=" h-[520px] relative flex flex-col grow gap-16 pt-14 px-24 gcr-rounded-products-wrapper gcr-rounded-wrapper border border-gray-900 mb-12 bg-light-grey">
         <div className="gcr-products-top-description text-2xl ">
           At Greencare Rwanda, we offer a range of competitive products that
           provide solutions to the agricultural sector, environmental
@@ -67,13 +69,13 @@ const OurProducts = () => {
         <div className="flex gap-14 absolute top-52 left-0 px-24">
           {products.map((product, index) => {
             return (
-              <div key={index} className={`flex flex-col flex-1 gap-4 bg-white gcr-rounded-product-wrapper mb-12 h-fit`}>
+              <div key={index} onClick={()=>setSelectedProduct(product)} className={`flex flex-col flex-1 cursor-pointer ${selectedProduct.productName===product.productName&&`gap-4 bg-white gcr-rounded-product-wrapper mb-12 h-fit`}`}>
                 <div className="flex flex-col items-center gap-4 px-10 pt-5">
                   <Image src={product.image} alt={product.productName} />
                   <div className="gcr-products-product-name">{product.productName}</div>
-                  <div className="gcr-products-product-description">{product.description}</div>
+                  <div className={`gcr-products-product-description ${selectedProduct.productName===product.productName? `block`:`hidden`}`}>{product.description}</div>
                 </div>
-                <div className="grid justify-items-stretch pb-5 pr-10">
+                <div className={`grid justify-items-stretch pb-5 pr-10 ${selectedProduct.productName===product.productName? `block`:`hidden`}`}>
                   <Image
                     className="justify-self-end"
                     src={ArrowRightIcon}
