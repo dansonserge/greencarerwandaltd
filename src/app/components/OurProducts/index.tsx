0ccfba42d7002222bb0bb21b8316452226f6cc30
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-08-13 13:22:20 +0200

    responsiveness on our products

--- src/app/components/OurProducts/index.tsx
+++ src/app/components/OurProducts/index.tsx
@@ -14,14 +14,24 @@ const OurProducts = () => {
   );
 
   return (
-    <div className="flex mx-20">
-      <div className="w-[800px] mt-10">
-        <div className="absolute z-10">
-        <div className="gradient-accent-color font-black text-2xl mb-5">Our products</div>
-        <div className="text-dark-blue font-black text-6xl z-10 w-12 sm:text-5xl h-32">{selectedProduct.productName}</div>
-        <div>
-          <div className="flex gap-5 m-5">
-            {products.map((item, index) =>
+    <div className="flex flex-col xl:flex-row mx-5 xl:mx-20">
+      <span className="xl:hidden block">
+        <div className="gradient-accent-color font-black text-3xl mb-5">
+          Our products
+        </div>
+      </span>
+
+      <div className="w-[800px] mt-10 hidden xl:block">
+        <div className="absolute z-10 ">
+          <div className="gradient-accent-color font-black text-2xl mb-5">
+            Our products
+          </div>
+          <div className="text-dark-blue font-black z-10 w-12 text-4xl h-32">
+            {selectedProduct.productName}
+          </div>
+          <div>
+            <div className="flex gap-5 m-5">
+              {products.map((item, index) =>
                 item.productName === selectedProduct.productName ? (
                   <Image
                     key={index}
@@ -44,34 +54,57 @@ const OurProducts = () => {
                   />
                 )
               )}
+            </div>
+
+            <CustomButton
+              text="Contact us"
+              type={"gradient"}
+              icon={ContactUsIcon}
+            />
           </div>
-
-          <CustomButton
-            text="Contact us"
-            type={"gradient"}
-            icon={ContactUsIcon}
-          />
-        </div>
-
         </div>
       </div>
-      <div className=" h-[520px] relative flex flex-col grow gap-16 pt-14 px-24 gcr-rounded-products-wrapper gcr-rounded-wrapper border border-gray-900 mb-12 bg-light-grey">
-        <div className="gcr-products-top-description text-2xl ">
-          At Greencare Rwanda, we offer a range of competitive products that
-          provide solutions to the agricultural sector, environmental
-          protection, and construction projects. Our products are designed to
-          promote a greener future while meeting the highest quality standards.
+      <div className="gcr-rounded-products-wrapper gcr-rounded-wrapper border border-gray-900 lg:mb-16 bg-light-grey flex flex-col py-5 px-8 xl:px-12 md:h-[520px] lg:pt-14 relative  lg:grow lg:gap-8 ">
+        <div className="gcr-products-top-description text-xl lg:text-2xl text-[#3C3F40]">
+          At <b>Greencare Rwanda Ltd</b>, we offer a range of competitive
+          products that provide solutions to the agricultural sector,
+          environmental protection, and construction projects. Our products are
+          designed to promote a greener future while meeting the highest quality
+          standards.
         </div>
-        <div className="flex gap-14 absolute top-52 left-0 px-24">
+        <div className="flex flex-col md:flex-row top-52 left-0 lg:px-4 xl:gap-14 xl:px-18">
           {products.map((product, index) => {
             return (
-              <div key={index} onClick={()=>setSelectedProduct(product)} className={`flex flex-col flex-1 cursor-pointer ${selectedProduct.productName===product.productName&&`gap-4 bg-white gcr-rounded-product-wrapper mb-12 h-fit`}`}>
-                <div className="flex flex-col items-center gap-4 px-10 pt-5">
+              <div
+                key={index}
+                onClick={() => setSelectedProduct(product)}
+                className={`flex flex-col flex-1 cursor-pointer ${
+                  selectedProduct.productName === product.productName &&
+                  `gap-4 bg-white gcr-rounded-product-wrapper mt-5 mb-6 xl:mb-12 h-fit`
+                }`}
+              >
+                <div className="flex flex-col items-center gap-4 px-5 pt-5">
                   <Image src={product.image} alt={product.productName} />
-                  <div className="gcr-products-product-name">{product.productName}</div>
-                  <div className={`gcr-products-product-description ${selectedProduct.productName===product.productName? `block`:`hidden`}`}>{product.description}</div>
+                  <div className="gcr-products-product-name">
+                    {product.productName}
+                  </div>
+                  <div
+                    className={`gcr-products-product-description ${
+                      selectedProduct.productName === product.productName
+                        ? `block`
+                        : `hidden`
+                    }`}
+                  >
+                    {product.description}
+                  </div>
                 </div>
-                <div className={`grid justify-items-stretch pb-5 pr-10 ${selectedProduct.productName===product.productName? `block`:`hidden`}`}>
+                <div
+                  className={`grid justify-items-stretch pb-5 pr-10 ${
+                    selectedProduct.productName === product.productName
+                      ? `block`
+                      : `hidden`
+                  }`}
+                >
                   <Image
                     className="justify-self-end"
                     src={ArrowRightIcon}
