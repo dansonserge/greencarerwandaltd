9ca994b5419bcdf20a697821f25d3734e6d65453
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-08-13 14:39:22 +0200

    responsiveness on the consultancy section

--- src/app/components/BookOurConsultancy.tsx
+++ src/app/components/BookOurConsultancy.tsx
@@ -6,7 +6,11 @@ import ContactUsIcon from "@/assets/contactUsIcon.svg";
 
 function BookOurConsultancy() {
   return (
-    <div className="mt-[150px] flex items-center gap-8 w-4/5 ">
+    <div className="mt-[150px] flex flex-col md:flex-row items-center gap-8 w-full md:w-4/5 ">
+      <div className="ml-5 md:ml-0 text-dark-blue font-bold text-4xl md:text-[45px] md:w-[450px] md:hidden">
+        Book our <span className="gradient-accent-color">consultancy</span>{" "}
+        services Today{" "}
+      </div>
       <div className="flex flex-1">
         <Image
           className="ml-auto"
@@ -15,18 +19,18 @@ function BookOurConsultancy() {
         />
       </div>
       <div className="flex flex-col gap-8 flex-1 h-full">
-        <div className="text-dark-blue font-bold text-[45px] w-[450px]">
+        <div className="text-dark-blue font-bold text-[45px] w-[450px] hidden md:block">
           Book our <span className="gradient-accent-color">consultancy</span>{" "}
           services Today{" "}
         </div>
-        <div className="font-[400] text-[#272727] text-xl">
+        <div className="font-[400] text-[#272727] text-xl text-center mx-5 md:mx-0 md:text-left">
           We provide a consultant in waste management by developing the best
           model for waste management and composting system for good quality,
           project assistant in solid waste management, advise in agriculture,
           recyclable materials treatments.
         </div>
         <div className="relative">
-          <div className="absolute mt-[20px] right-0">
+          <div className="absolute md:mt-[20px right-5 md:right-0">
             <CustomButton
               text="Contact us"
               type={"gradient-right-arrow"}
