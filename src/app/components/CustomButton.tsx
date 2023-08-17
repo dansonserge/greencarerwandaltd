e75b1c75e525f43e75b27730bbe644f87aeb30f6
Serge Danson Ndekezi <sergedanson@gmail.com>
2023-08-17 20:25:36 +0200

    validate the send email

--- src/app/components/CustomButton.tsx
+++ src/app/components/CustomButton.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 
@@ -6,50 +5,53 @@ const CustomButton = ({
   text,
   type,
   icon,
+  handleOnClick,
 }: {
   text: string;
   type: string;
   icon?: string;
+  handleOnClick?: () => void;
 }) => {
   return (
     <>
       {type === "normal" && (
-        <Link
-          href={""}
+        <button
+          onClick={() => handleOnClick}
           className="customButtonOne flex items-center justify-center gap-2"
         >
           {text}
           {icon && <Image src={icon} alt={text} />}
-        </Link>
+        </button>
       )}
       {type === "normal-right" && (
-        <Link
-          href={""}
+        <button
+          onClick={() => handleOnClick}
           className="customButtonFour flex items-center justify-center gap-2"
         >
           {text}
           {icon && <Image src={icon} alt={text} />}
-        </Link>
+        </button>
       )}
 
       {type === "gradient" && (
-        <Link
-          href={""}
+        <button
+
+          onClick={() => handleOnClick}
           className="customButtonTwo flex items-center justify-center gap-1"
         >
           <span className="gradient-accent-color font-bold">Contact us</span>
           {icon && <Image src={icon} alt={text} />}
-        </Link>
+        </button>
       )}
 
       {type === "gradient-right-arrow" && (
-        <Link
-          href={""}
+        <button
+          onClick={handleOnClick}
           className="customButtonThree flex items-center justify-center gap-1"
         >
           <span className="gradient-accent-color font-bold">Contact us</span>
           {icon && <Image src={icon} alt={text} />}
-        </Link>
+        </button>
       )}
     </>
   );
--- src/app/components/Footer/ContactUsForm.tsx
+++ src/app/components/Footer/ContactUsForm.tsx
@@ -1,39 +1,142 @@
-import React from 'react'
-import CustomButton from '../CustomButton';
+import React, { useState } from "react";
+import CustomButton from "../CustomButton";
 import SendMessageIcon from "@/assets/send-message-icon.svg";
 
 function ContactUsForm() {
+  interface FormDataType {
+    names: string;
+    email: string;
+    phone: string;
+    message: string;
+  }
+  interface TouchedFieldsType {
+    names: boolean;
+    email: boolean;
+    phone: boolean;
+    message: boolean;
+  }
+
+  const initValues = {
+    names: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+  const initTouchedValues = {
+    names: false,
+    email: false,
+    phone: false,
+    message: false,
+  };
+
+  const initState: { values: FormDataType } = { values: initValues };
+
+  const [formData, setFormData] = useState(initState);
+  const [touched, setTouched] = useState<TouchedFieldsType>(initTouchedValues);
+
+  const { values } = formData;
+
+  const handleSendEmail = () => {
+    console.log("sending the email");
+  };
+
+  const onBlurHandler = ({ target }) =>
+    setTouched((prev) => ({ ...prev, [target.name]: true }));
+
+  const handleChange = ({ target }) =>
+    setFormData((prev) => ({
+      ...prev.values,
+      values: { ...prev.values, [target.name]: target.value },
+    }));
+
   return (
     <div className="bg-white px-4 md:px-10 py-5  contactUsForm w-full lg:w-5/6">
-    <p className="ml-2 gradient-accent-color font-[700] text-2xl py-4">Contact us now</p>
-    <form className="flex flex-col gap-5">
-      <div className="flex flex-col gap-2">
-        <label htmlFor="name" className="ml-2 font-[700]">Name</label>
-        <input type="text" name="names" className="contactUsInput"></input>
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="name" className="ml-2 font-[700]">Email</label>
-        <input type="email" name="email" className="contactUsInput"></input>
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="name" className="ml-2 font-[700]">Phone</label>
-        <input type="phone" name="phone" className="contactUsInput"></input>
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="name" className="ml-2 font-[700]">Message</label>
-        <textarea name="message" className="contactUsTeaxtArea"></textarea>
-      </div>
+      <p className="ml-2 gradient-accent-color font-[700] text-2xl py-4">
+        Contact us now
+      </p>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="ml-2 font-[700]">
+            Name <span className=" text-red-400">*</span>
+          </label>
+          <input
+            type="text"
+            name="names"
+            className={`contactUsInput border ${
+              touched.names && values.names.length === 0 && "border-red-400"
+            }`}
+            value={values.names}
+            onChange={handleChange}
+            onBlur={onBlurHandler}
+          ></input>
+          {touched.names && values.names.length === 0 && (
+            <span className="ml-2 text-red-400">Required</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="ml-2 font-[700]">
+            Email <span className=" text-red-400">*</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            className={`contactUsInput border ${
+              touched.email && values.email.length === 0 && "border-red-400"
+            }`}
+            value={values.email}
+            onChange={handleChange}
+            onBlur={onBlurHandler}
+          ></input>
+          {touched.email && values.email.length === 0 && (
+            <span className="ml-2 text-red-400">Required</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="ml-2 font-[700]">
+            Phone <span className=" text-red-400">*</span>
+          </label>
+          <input
+            type="phone"
+            name="phone"
+            className={`contactUsInput border ${
+              touched.phone && values.phone.length === 0 && "border-red-400"
+            }`}
+            value={values.phone}
+            onChange={handleChange}
+            onBlur={onBlurHandler}
+          ></input>
+          {touched.phone && values.phone.length === 0 && (
+            <span className="ml-2 text-red-400">Required</span>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name" className="ml-2 font-[700]">
+            Message <span className=" text-red-400">*</span>
+          </label>
+          <textarea
+            name="message"
+            className={`contactUsTeaxtArea border ${
+              touched.message && values.message.length === 0 && "border-red-400"
+            }`}
+            value={values.message}
+            onChange={handleChange}
+            onBlur={onBlurHandler}
+          ></textarea>
+          {touched.message && values.message.length === 0 && (
+            <span className="ml-2 text-red-400">Required</span>
+          )}
+        </div>
 
-      <div className="ml-auto">
-        <CustomButton
-          type="normal-right"
-          text="Send"
-          icon={SendMessageIcon}
-        />
+        <div className="ml-auto" onClick={() => handleSendEmail()}>
+          <CustomButton
+            type="normal-right"
+            text="Send"
+            icon={SendMessageIcon}
+          />
+        </div>
       </div>
-    </form>
-  </div>
-  )
+    </div>
+  );
 }
 
-export default ContactUsForm
+export default ContactUsForm;
